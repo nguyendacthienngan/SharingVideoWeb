c99c75a789840f23d02b653a8b5fa33f46f763cb
nguyendacthienngan <nguyendacthienngan@gmail.com>
2021-06-10 23:17:58 +0700

    Changing from Vue 3 to Vue 2 Adding bootstrapvue package

--- connexion-app/src/App.vue
+++ connexion-app/src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
@@ -23,4 +25,6 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+@import'~bootstrap/dist/css/bootstrap.css'
+
 </style>
