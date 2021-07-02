20cef00cdb34089e3ff7be756a27a4270a8f6e94
nguyendacthienngan <nguyendacthienngan@gmail.com>
2021-07-02 10:33:42 +0700

    Component NavBar completed

--- connexion-app/src/components/common/SearchBar.vue
+++ connexion-app/src/components/common/SearchBar.vue
@@ -6,23 +6,33 @@
 </template>
 
 <style scoped>
-#search_in_navbar{
-    /* border-radius: 30px; */
-    color: transparent;
-    border: 1px solid transparent;
-    background: #E7ECF0 !important;
-    width: 100%;
-}
+
 .input-container {
     background: #E7ECF0 !important;
     border-radius: 50px !important;
     border: 1px solid transparent !important;
-  }
+}
+.input-container:focus {
+  background-color: yellow !important;
+  border: 1px solid red !important;
+}
+#search_in_navbar{
+    /* border-radius: 30px; */
+    color: #828282;
+    border: 1px solid #E7ECF0;
+    background: #E7ECF0 !important;
+    width: 100%;
+}
+
+input[type="search"], input[type="text"], textarea, select { 
+   outline: none;
+}
+
   
-  .icon {
+.icon {
     color:  #828282;
     text-align: center;
     margin: auto;
     margin-right: 5px;
-  }
+}
 </style>
