4d79db71222f512161a66b7cceae3e0da7cb24af
nguyendacthienngan <nguyendacthienngan@gmail.com>
2021-07-03 02:58:25 +0700

    Adding social interaction buttons

--- connexion-app/src/views/Comment.vue
+++ connexion-app/src/views/Comment.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row comment-view">
         <div class="col-md-6 order-2 order-lg-1 left-video">
-            <router-link to="/">
+            <router-link to="/" class="d-none d-sm-block">
                  <span class="fa fa-times fa-2x exit-button"></span>
             </router-link>
             <img src="../assets/images/sample/Image_2.png" class="w-100">
@@ -15,8 +15,30 @@
                 <div class="card non-border">
                     <div class="card-header container bg-white ">
                         <PostCaption name="INTO1–刘宇" username="@into1_liuyu_" caption="Relatively cool" bg_music="Crazy Frog"></PostCaption>
+                        <router-link to="/" class="d-sm-none">
+                            <span class="fa fa-times fa-2x exit-button"></span>
+                        </router-link>
                         <br>
                     </div>
+                    <div class="card non-border">
+                        <div class="card-header container bg-white">
+                            <div class="row">
+                                <div class="col text-center">
+                                    <img src="../assets/images/Like-Icon.svg" width="48px" height="48px">
+                                    5555
+                                </div>
+                                <div class="col text-center">
+                                    <img src="../assets/images/Comment-Icon.svg" width="48px" height="48px">
+                                    5555
+                                </div>
+                                <div class="col text-center">
+                                    <img src="../assets/images/Share-Icon.svg" width="48px" height="48px">
+                                    5555
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
                     <div class="d-none d-sm-block">
                         <CommentSection></CommentSection>
                     </div>
