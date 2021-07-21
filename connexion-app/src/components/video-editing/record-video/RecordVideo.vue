f23ddf3923a1c2108e43dac721c5f33d5a6f02d1
nguyendacthienngan <nguyendacthienngan@gmail.com>
2021-07-22 00:45:44 +0700

    Styling record modal

--- connexion-app/src/components/video-editing/record-video/RecordVideo.vue
+++ connexion-app/src/components/video-editing/record-video/RecordVideo.vue
@@ -1,78 +1,97 @@
 <template>
     <div>
-        
-
-        <b-modal ref="recordVideo"  id="recordVideo" title="Record Video" >
-             <b-button id="show-btn" @click="showModal">Open Modal</b-button>
-        <b-button id="toggle-btn" @click="toggleModal">Toggle Modal</b-button>
-        <button id="start-camera" @click="recordVideo">Start Camera</button>
-            <video id="recorded-video" width="320" height="240" autoplay="autoplay" >
-                <source type="video/webm"/>
+        <b-modal ref="recordVideo"  id="recordVideo" title="Record Video" hide-header hide-footer>
+        <span id="toggle-btn" @click="hideModal">
+            <i class="fa fa-times fa-2x exit-button "></i>
+        </span>
+        <div class="w-100 text-center">
+            <button id="start-camera" class="btn btn-primary h-100" @click="recordVideo">Start Camera</button>
+            
+            <video id="recorded-video" autoplay="autoplay" muted >
+                <source type="video/webm" />
             </video>
-            <button id="start-record" @click="startRecord">Start Recording</button>
-            <button id="stop-record" @click="stopRecord">Stop Recording</button>
-
-            <a id="download-video" download="test.webm">Download Video</a>
-
+            
+        </div>
+        
+        <div v-if="isStarted" class="row">
+            <button class="btn btn-primary"  @click="hideModal">Cancel</button>
+            <img src="../../../assets/images/recording.svg" width="60px" height="55px" @click="startRecord"/>
+            
+            <!-- <a id="download-video" download="test.webm">Download Video</a> -->
+            <a id="download-video" download="test.webm" class="btn btn-primary">Download Video</a>
+            
+        </div>
+        
         </b-modal>
     </div>
 </template>
-
+<style scoped>
+    #toggle-btn{
+        float: right;
+    }
+    #recorded-video{
+        width: 100%    !important;
+        height: auto   !important;
+    }
+</style>
 <script>
-import $ from 'jquery';
   export default {
         data() {
             return {
                 camera_stream: null,
                 media_recorder: null,
-                blobs_recorded: []
+                blobs_recorded: [],
+                isRecording: false,
+                isStarted: false
             } 
         },
         methods: {
-        showModal() {
-            this.$refs['recordVideo'].show()
-        },
         hideModal() {
             this.$refs['recordVideo'].hide()
         },
-        toggleModal() {
-            // We pass the ID of the button that we want to return focus to
-            // when the modal has hidden
-            this.$refs['recordVideo'].toggle('#recordVideo')
-        },
         async recordVideo(){
-            
+            this.isStarted = true
             let recordVideo = document.getElementById('recorded-video');
             this.camera_stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
             recordVideo.srcObject = this.camera_stream;
         },
         startRecord(){
-            // set MIME type of recording as video/webm
-            this.media_recorder = new MediaRecorder(this.camera_stream, { mimeType: 'video/webm' });
+            if (this.isRecording == false)
+            {
+                this.isRecording = true
+                // set MIME type of recording as video/webm
+                this.media_recorder = new MediaRecorder(this.camera_stream, { mimeType: 'video/webm' });
 
-            // event : new recorded video blob available 
-            let vm = this;
+                // event : new recorded video blob available 
+                let vm = this;
 
-            this.media_recorder.addEventListener('dataavailable', function(e) {
-                vm.blobs_recorded.push(e.data);
-            });
+                this.media_recorder.addEventListener('dataavailable', function(e) {
+                    vm.blobs_recorded.push(e.data);
+                });
 
-            // event : recording stopped & all blobs sent
-            this.media_recorder.addEventListener('stop', function() {
-                // create local object URL from the recorded video blobs
-                let video_local = URL.createObjectURL(new Blob(vm.blobs_recorded, { type: 'video/webm' }));
-                let download_link = document.querySelector("#download-video");
+                // event : recording stopped & all blobs sent
+                this.media_recorder.addEventListener('stop', function() {
+                    // create local object URL from the recorded video blobs
+                    let video_local = URL.createObjectURL(new Blob(vm.blobs_recorded, { type: 'video/webm' }));
+                    let download_link = document.querySelector("#download-video");
 
-                download_link.href = video_local;
-            });
+                    download_link.href = video_local;
+                });
 
-            // start recording with each recorded blob having 1 second video
-            this.media_recorder.start(1000);
+                // start recording with each recorded blob having 1 second video
+                this.media_recorder.start(1000);
+            }
+            else
+            {
+                alert("STOPPED")
+                this.media_recorder.stop(); 
+                let recordVideo = document.getElementById('recorded-video');
+                recordVideo.pause();
+            }
         },
         stopRecord(){
             this.media_recorder.stop(); 
         }
-    },
-
+    }
   }
 </script>
