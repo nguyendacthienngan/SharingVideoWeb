c90a125dc687c0249687d04b070a375283b9a4a7
nguyendacthienngan <nguyendacthienngan@gmail.com>
2021-07-24 14:28:32 +0700

    Scroll To Top when click on hashtag

--- connexion-app/src/components/discover/Trending.vue
+++ connexion-app/src/components/discover/Trending.vue
@@ -11,6 +11,25 @@ import Trend from './Trend.vue'
 export default {
     components: {
         Trend,
+    },
+    methods:{
+        scrollToTop() {
+            window.scrollTo(0, 0);
+        },
+    },
+    watch: {
+        '$route.params.id':{
+            handler: function(search) {
+                this.scrollToTop()
+            },
+            deep: true,
+            immediate: true
+        }
+    },
+    mounted(){
+        this.$nextTick(() => {
+            this.scrollToTop()
+        }) 
     }
 }
 </script>
